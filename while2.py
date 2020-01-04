d54ab007c6da040cb76e1f8b67b4f5941ad9a429
Min Cheung <ilove52345234@gmail.com>
2020-01-04 20:52:24 +0800

    updata3 while2

--- while2.py
+++ while2.py
@@ -1,12 +1,14 @@
 x = 3
 pwd = 'a123456'
-while True:
+while x > 0:
 	password = input('請輸入密碼: ')
 	x = x - 1
-	if password != pwd and x > 0:
-		print('密碼錯誤還有', x, '次機會')
-	elif password == pwd:
+	if password == pwd:
 		print('登入成功')
 		break
-	elif x == 0:
-		break
+	else:
+		print('密碼錯誤')
+		if x > 0:
+			print('還有', x, '次機會')
+		else:
+			print('請聯絡客服人員!')
